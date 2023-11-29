69ec1d6db62960cd3ee744aa55d8fb19512f6aaa
Rebecca Kiefer <rebeccakiefer8@gmail.com>
2023-11-29 15:42:57 -0600

    initialize game

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 // ===================================================== 
+//                  CONSTANTS
+// ===================================================== 
 //  Define required constants:
 //   - Master list of secret words
 //   - Letter states: exact match, partial match, no match, unknown (not tried) with their associated background colors
@@ -19,16 +21,15 @@ const LETTER_STATE_LOOKUP = {
 };
 
 const MAX_GUESSES = 6;
+const WORD_LENGTH = 5;
 
 
 // ===================================================== 
-//  Identify and initialize state variables:
-//   - Secret word
-//   - Player object to hold stats
-//   - Guesses array of arrays - store each letter separately to render easier
-//   - Letters array - store current state of each letter
-//   -
+//                  CLASSES
 // ===================================================== 
+//  Class for Player which stores and updates statistics
+// ===================================================== 
+
 class Player {
     constructor() {
         this.winCount = 0;
@@ -47,14 +48,29 @@ class Player {
     }
 }
 
-const player = new Player();   // instance of Player to hold the statistics for the current user
+
+// ===================================================== 
+//                  STATE VARIABLES
+// ===================================================== 
+//  Identify and initialize state variables:
+//   - Secret word
+//   - Player object to hold stats
+//   - Guesses array of arrays - store each letter separately to render easier
+//   - Letters array - store current state of each letter
+//   -
+// ===================================================== 
+
+let player;   // instance of Player to hold the statistics for the current user
 
 let secretWord;   // secret word
 
 let guesses;  // an array of 6 nested arrays containing objects
 let letters;  // an array of 26 arrays where the key is the letter (from keyboard) and the value is it's letter state
 
+let numGuesses;  // the number of guesses the player has made already
 
+// ===================================================== 
+//                  DOM ELEMENTS
 // ===================================================== 
 // Store elements that will be needed multiple times
 //   - Guess button
@@ -74,6 +90,54 @@ console.log('[play again button] element: \n', playAgainButtonEl);
 // grab keyboard elements and save them to an array
 const keysEls = [...document.querySelectorAll(".row > div")];
 console.log('keys element array: \n', keysEls);
+
+
+// ===================================================== 
+//                 FUNCTIONS
+// ===================================================== 
+
+// Initialize the game:
+function init() {
+    // if a player object does not exist, create one; otherwise, assume it's the same player
+    console.log('\ninit: player \n', player);
+    if (!player) player = new Player();
+    console.log('\ninit: player \n', player);
+
+    numGuesses = 0;
+
+    // pick secret word
+    secretWord = getSecretWord();
+
+    // reset guesses array
+    console.log('init: guesses before reset():\n', guesses);
+    resetGuesses();
+    console.log('init: guesses after reset():\n', guesses);
+
+    // reset letters state
+    letters = [];
+}
+
+init();
+
+// Get Secret Word - Select a random word from the master array and return it
+function getSecretWord() {
+    const wordIdx = Math.floor(Math.random() * SECRET_WORD_LIST.length);
+    console.log('\n getSecretWord() - secretWord: \n', SECRET_WORD_LIST[wordIdx]);
+
+    return SECRET_WORD_LIST[wordIdx];
+}
+
+// Reset Guesses Array - Reinitialize guesses to be a two-dimensional array
+//  of objects - initially containing just a state of 0 (or unknown)
+function resetGuesses() {
+    guesses = new Array(MAX_GUESSES);
+    for (let i = 0; i < MAX_GUESSES; i++) {
+        guesses[i] = [];
+        for (let j = 0; j < WORD_LENGTH; j++) {
+            guesses[i][j] = { state: 0 };
+        }
+    }
+}
 
 // ===================================================== 
 // Render the screen:
