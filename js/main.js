ece4014e6a98a12e265aa76d622de297bc89a33d
Rebecca Kiefer <rebeccakiefer8@gmail.com>
2023-11-29 13:44:54 -0600

    add pseudocode to js, modify screepositioning

--- js/main.js
+++ js/main.js
@@ -1,1 +1,82 @@
-console.log('js is linked and working');
+// ===================================================== 
+//  Define required constants:
+//   - Master list of secret words
+//   - Letter states: exact match, partial match, no match, unknown (not tried) with their associated background colors
+//       = Exact Match ==> correct letter in the correct spot (green)
+//       = Partial Match ==> correct letter in the wrong spot (yellow)
+//       = No Match ==> letter not in secret word (grey)
+//       = Unknown ==> letter has not been tried yet (white)
+// ===================================================== 
+
+// ===================================================== 
+//  Identify and initialize state variables:
+//   - Secret word
+//   - Player object to hold stats
+//   - Guesses array of arrays - store each letter separately to render easier
+//   - Letters array - store current state of each letter
+// ===================================================== 
+
+// ===================================================== 
+// Store elements that will be needed multiple times
+//   - Guess button
+//   - Play Again button
+//   - Guess square divs
+// ===================================================== 
+
+// ===================================================== 
+// Render the screen:
+//   - Render the appropriate message (make a guess, you win, you lose)
+//   - Render the guesses with the appropriate background color based on the guess's state (exact match, partial match, no match, unknown - not tried)
+//   - Render the screen keyboard with the appropriate background color based on the letter's state (exact match, partial match, no match, unknown - not tried)
+//   - Render the button: "GUESS" if user has more tries or "PLAY AGAIN" if they lost
+// ===================================================== 
+
+// ===================================================== 
+// Handle Click of Screen "Keyboard" Buttons:
+//   - Check if "letter" button clicked - if not, ignore click
+//   - Capture "letter" of button clicked
+//   - Call Handle Selected Letter (below) with letter clicked
+// ===================================================== 
+
+// ===================================================== 
+// Handle Press of Keyboard
+//   - Check if key pressed is a letter - if not, ignore press
+//   - Capture "letter" of key pressed
+//   - Call Handle Selected Letter (below) with letter pressed
+// ===================================================== 
+
+// ===================================================== 
+// Handle Selected Letter
+//   - Update current guess square state variable with letter clicked
+//   - Advance to next letter square on current guess
+//   - If no more empty squares on current guess, highlight "GUESS" button
+//   - Render changes to screen
+// ===================================================== 
+
+// ===================================================== 
+// Handle Click of "GUESS" button:
+//   - Check for win - does guess equal mystery word
+//   - If no win, for each letter in guess, 
+//      = Check if letter at that spot is same as secret word at that spot.  If so, mark letter as "exact match"
+//      = Check if letter at that spot is anywhere else in secret word.  If so, mark letter as "found but not exact match"
+//      = Otherwise, mark letter as "not in secret word"
+//   - Increment number of guesses
+//   - If win or lose, update statistics
+// ===================================================== 
+
+// ===================================================== 
+// Handle Click of "PLAY AGAIN" button:
+//   - Reinitialize game
+// ===================================================== 
+
+// ===================================================== 
+// Handle Click of "Stats" button:
+//   - Display Number of Games Played
+//   - Display Win %
+//   - Display Current Streak
+// ===================================================== 
+
+// ===================================================== 
+// Handle Click of "Help" button:
+//   - Overlay the instructions on screen
+// ===================================================== 
